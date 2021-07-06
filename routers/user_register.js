34f2439706b8166a1773df63abb1be8aec55834d
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-07-06 19:36:18 +0300

    Create stored procedure RegisterUser

--- routers/user_register.js
+++ routers/user_register.js
@@ -30,10 +30,7 @@ router.post('/register', async function(req, res, next) {
         .input("First_Name", sql.NVarChar, req.body.First_Name)
         .input("Last_Name", sql.NVarChar, req.body.Last_Name)
         .input("Password", sql.VarChar, passHash)
-        .query(`
-            INSERT INTO Users (Username, First_Name, Last_Name, Password)
-            VALUES (@Username, @First_Name, @Last_Name, @Password)
-        `)
+        .execute("RegisterUser");
         console.log(result)
 
     } catch (err) {
