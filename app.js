9e867c8f9a4761b8cdd79c1bfcee7e43836a7a36
GSKalchev18 <GSKalchev18@codingburgas.bg>
2021-07-07 09:36:58 +0300

    Add error_page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5500;
 
 let users_list_router = require('./routers/users_list');
 let user_registration_router = require('./routers/user_register');
+let login_page_router = require('./routers/login');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -12,6 +13,23 @@ app.use(express.json());
 
 app.use('/', users_list_router);
 app.use('/', user_registration_router);
+app.use('/', login_page_router);
+
+// Catch 404 and forward to error handler
+app.use('*', function(req, res, next) {
+    next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+    // Set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // Render the error page
+    res.status(err.status || 500);
+    res.render('error_page');
+});
 
 app.listen(PORT, function(req, res) {
     console.log(`Server is running on port ${PORT}`);
