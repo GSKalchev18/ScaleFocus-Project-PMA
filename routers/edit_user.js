9eda8d920acbff29d7b6d7729c868a0aec31ff45
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-07-07 19:40:43 +0300

    temporary edit user form

--- routers/edit_user.js
+++ routers/edit_user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {config} = require('../config/database_config');
 const sql = require('mssql/msnodesqlv8');
-const crypto = require('crypto');
+//const crypto = require('crypto');
 let bodyParser = require('body-parser');
 
 let router = express.Router();
@@ -28,22 +28,23 @@ router.use(bodyParser.urlencoded({ extended: true }));
     }
 })()
 
-const sha256 = crypto.createHash('sha256');
+//const sha256 = crypto.createHash('sha256');
 
 router.post('/edit', async function(req, res, next) {
     try {
         // Put data into the Sql server
         const pool = await sql.connect(config);
-        const passHash = sha256.update(req.body.Password).digest('hex');
+        //const passHash = sha256.update(req.body.Password).digest('hex');
         
         const result = await pool.request()
         .input("Username", sql.VarChar, req.body.Username)
         .input("First_Name", sql.NVarChar, req.body.First_Name)
         .input("Last_Name", sql.NVarChar, req.body.Last_Name)
-        .input("Password", sql.VarChar, passHash)
+        //.input("Password", sql.VarChar, passHash)
+        .input("Id", sql.Int, req.body.Id)
         .query(`UPDATE Users 
-                SET Username = @Username, First_Name = @First_Name, Last_Name = @Last_Name, Password = @Password
-                WHERE Id = 1`);
+                SET Username = @Username, First_Name = @First_Name, Last_Name = @Last_Name
+                WHERE Id = @Id`);
         console.log(result)
 
     } catch (err) {
