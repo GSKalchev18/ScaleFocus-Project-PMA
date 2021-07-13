f9cf6448aa3d7e7b3cd512b06248b38e0efd3a1f
GSKalchev18 <GSKalchev18@codingburgas.bg>
2021-07-13 09:54:31 +0300

    Create vUsersTeams view

--- routers/teams_list.js
+++ routers/teams_list.js
@@ -8,15 +8,12 @@ let router = express.Router();
     try{
         let connection = await sql.connect(config);
         const teams_result = await connection.request()
-        .query(`SELECT Teams.Id, Teams.Title, Teams.DateOfCreation FROM Teams`);
-
-        const user_result = await connection.request()
-        .query(`SELECT Users.First_Name, Users.Last_Name FROM Users`);
+        .query(`SELECT * FROM vUsersTeams`);
 
         router.get('/teams', function(req, res) {
             if (req.session.isAdmin == true) {
                 res.render('teams_list', 
-                {TeamsList: teams_result.recordset, UserList: user_result.recordset});
+                {TeamsList: teams_result.recordset});
             } else {
                 res.render('error_page');
             }
